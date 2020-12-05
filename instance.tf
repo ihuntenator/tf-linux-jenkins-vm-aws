e67f3d734061ba136103b82fffe798cc2e28ee4e
Ian hunt <ihuntenator@gmail.com>
2020-12-05 16:48:41 +1300

    first commit

---
+++ instance.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "jenkins" {
+  ami             = lookup(var.amis, var.region) 
+  subnet_id       = var.subnet 
+  security_groups = var.securityGroups 
+  key_name        = var.keyName 
+  instance_type   = var.instanceType 
+  
+  tags = {
+    Name = var.instanceName
+  }
+
+  volume_tags = {
+    Name = var.instanceName
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "centos"
+    private_key = file(var.keyPath)
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+      "sudo yum upgrade",
+      "sudo yum install jenkins java-1.8.0-openjdk-devel -y",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl start jenkins",
+      "sudo yum install git -y",
+      "sudo service jenkins start",
+      "sudo chkconfig jenkins on",
+    ]
+  }
+} 
